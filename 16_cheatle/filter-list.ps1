8e7a5be4618e1778dbf9c4b193b96dbb5c5ec1b8
Chris Guest <chris.guest@trint.com>
2022-01-19 23:05:39 +0000

    cheatle first checkin

---
+++ 16_cheatle/filter-list.ps1
@@ -0,0 +1,89 @@
+#!/usr/bin/env pwsh
+
+<#
+.Synopsis
+
+.Description
+
+.Parameter help
+Show help
+
+.Example
+./filter-list.ps1 -filter -min 4 -max 8 -path ./words.txt
+./filter-list.ps1 -filter -min 5 -max 5 -path ./words.txt
+#>
+param(
+    [Parameter(Mandatory=$false)][switch]$help=$false,
+    [Parameter(Mandatory=$false)][switch]$filter=$false,
+    [Parameter(Mandatory=$false)][int]$min=5,
+    [Parameter(Mandatory=$false)][int]$max=5,
+    [Parameter(Mandatory=$false)][string]$path=""
+)
+
+#***********************************************************************************************************
+#** Usage
+#***********************************************************************************************************
+
+$showhelp = $false
+
+if ( $PSBoundParameters.Values.Count -eq 0 ) {
+    $showhelp = $true
+}
+
+if(($help -eq $true) -or ($showhelp -eq $true))
+{
+    Get-Help $MyInvocation.MyCommand.Definition -Detailed
+    return
+}
+
+if ($filter) {
+    $wordsfile = get-content $path
+    $filtered = ($wordsfile | where { ($_.length -le $max) -and ($_.length -ge $min) })
+
+    $nodoubles = ($filtered | select-string -notmatch "(.)\1")
+    [System.Collections.ArrayList]$words = @()
+    $noduplicates = ($nodoubles | foreach-object {
+        $letter=@{}
+        $continue = $true
+        $current=$_
+        [char[]]"$current" | foreach-object {
+            $c=[string]$_
+            if ($letter.contains($c)) {
+                $continue = $false
+            } else {
+                $letter.add($c, $true) 
+            }
+        }
+        if ($continue) {
+            $vowels = 0
+            if ($letter.contains('a')) {
+                $vowels=$vowels+1
+            }
+            if ($letter.contains('e')) {
+                $vowels=$vowels+1
+            }
+            if ($letter.contains('i')) {
+                $vowels=$vowels+1
+            }
+            if ($letter.contains('o')) {
+                $vowels=$vowels+1
+            }
+            if ($letter.contains('u')) {
+                $vowels=$vowels+1
+            }
+
+            $word = [PSCustomObject]@{
+                word = $_
+                vowels = $vowels
+                #letter = $letter
+                }
+            $word 
+        }
+    })
+    #$noduplicates | format-table -autosize
+    #$filtered.length
+    #$nodoubles.length
+    #$noduplicates.length
+
+    $noduplicates | sort-object vowels
+}
