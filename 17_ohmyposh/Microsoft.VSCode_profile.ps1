3bf4f5cd091175dd7556b8a92adc1f6e220b7fb0
Chris Guest <chris.guest@trint.com>
2022-01-22 23:29:19 +0000

    Remove comments.

--- 17_ohmyposh/Microsoft.VSCode_profile.ps1
+++ 17_ohmyposh/Microsoft.VSCode_profile.ps1
@@ -1,9 +1,6 @@
 Import-Module oh-my-posh
 Set-PoshPrompt -Theme chrisguest
 #Set-PoshPrompt -Theme powerline
-#Set-PoshPrompt -Theme powerline_chrisguest
-# Set-PoshPrompt -Theme powerlevel10k_classic
-#Set-PoshPrompt -Theme powerlevel10k_chrisguest
 
 Import-Module -Name Terminal-Icons
 
