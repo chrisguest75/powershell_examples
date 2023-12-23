5ac1823c8acb7343d6754639ed950edda5bd2acf
Chris Guest <chrisguest75@users.noreply.github.com>
2023-12-23 14:49:32 +0000

    Wrap up pode web example

--- 20_pode_web/server.ps1
+++ 20_pode_web/server.ps1
@@ -4,30 +4,17 @@ Import-Module Pode.Web
 Start-PodeServer {
     # add a simple endpoint
     Add-PodeEndpoint -Address "0.0.0.0" -Port 8080 -Protocol Http
-    New-PodeLoggingMethod -Terminal | Enable-PodeErrorLogging
 
-    # set the use of templates, and set a login page
-    Use-PodeWebTemplates -Title 'Basic Example' -Theme Dark
+    # set the use of the pode.web templates
+    Use-PodeWebTemplates -Title 'Example' -Theme Dark
 
-    # social
-    Set-PodeWebSocial -Type GitHub -Url 'https://github.com/badgerati'
-    Set-PodeWebSocial -Type Twitter -Url 'https://twitter.com/Badgerati' -Tooltip '@Badgerati'
-
-    # set the home page controls (just a simple paragraph)
-    $section = New-PodeWebCard -Name 'Welcome' -NoTitle -Content @(
-        New-PodeWebParagraph -Value 'This is an example homepage, with some example text'
-        New-PodeWebParagraph -Value 'Using some example paragraphs'
-    )
-    Set-PodeWebHomePage -Layouts $section -Title 'Awesome Homepage'
-
-    # add a page to search process (output as json in an appended textbox)
-    $form = New-PodeWebForm -Name 'Search' -AsCard -ScriptBlock {
-        Get-Process -Name $WebEvent.Data.Name -ErrorAction Ignore | Select-Object Name, ID, WorkingSet, CPU | Out-PodeWebTextbox -Multiline -Preformat -AsJson
-    } -Content @(
-        New-PodeWebTextbox -Name 'Name'
-    )
-
-    Add-PodeWebPage -Name Processes -Icon 'chart-box-outline' -Layouts $form
-
-    Use-PodeWebPages
+    # add the page
+    Add-PodeWebPage -Name Processes -Icon Activity -ScriptBlock {
+        New-PodeWebChart -Name 'Top Processes' -Type Bar -AutoRefresh -AsCard -ScriptBlock {
+            Get-Process |
+                Sort-Object -Property CPU -Descending |
+                Select-Object -First 10 |
+                ConvertTo-PodeWebChartData -LabelProperty ProcessName -DatasetProperty CPU
+        }
+    }
 }
